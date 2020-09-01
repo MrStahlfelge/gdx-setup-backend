12667ff95d5261a25ff0e48c47d8e8589bbfeb6c
Benjamin Schulte <bs@golfgl.de>
2020-09-01 19:33:58 +0200

    Initial commit, freshly generated Spring Boot application

---
+++ src/main/java/com/badlogic/gdx/setup/GdxSetupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.badlogic.gdx.setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdxSetupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdxSetupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badlogic/gdx/setup/GdxSetupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badlogic.gdx.setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdxSetupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
